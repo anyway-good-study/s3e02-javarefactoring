87b205dff5271cedf8397a905aac0aeb56cd3fe0
JangYoungWhan <stdjangyoungwhan@gmail.com>
2018-04-11 22:09:34 +0900

    분류 코드를 하위 클래스로 치환

--- 11_replace_constructor_with_factory_method/src/StudyJava.java
+++ 11_replace_constructor_with_factory_method/src/StudyJava.java
@@ -1,43 +1,78 @@
 
-class Shape {
-    public static final int TYPECODE_LINE = 0;
-    public static final int TYPECODE_RECTANGLE = 1;
-    public static final int TYPECODE_OVAL = 2;
+class ShapeLine extends Shape {
+    public ShapeLine(int startx, int starty, int endx, int endy) {
+        super(startx, starty, endx, endy);
+    }
 
-    private final int typecode;
+    @Override
+    public String getName() {
+        return "LINE";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("drawLine: " + this.toString());
+    }
+}
+
+class ShapeRectangle extends Shape {
+    public ShapeRectangle(int startx, int starty, int endx, int endy) {
+        super(startx, starty, endx, endy);
+    }
+
+    @Override
+    public String getName() {
+        return "RECTANGLE";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("drawRectangle: " + this.toString());
+    }
+}
+
+class ShapeOval extends Shape {
+    public ShapeOval(int startx, int starty, int endx, int endy) {
+        super(startx, starty, endx, endy);
+    }
+
+    @Override
+    public String getName() {
+        return "OVAL";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("drawOval: " + this.toString());
+    }
+}
+
+abstract class Shape {
     private final int sx;
     private final int sy;
     private final int ex;
     private final int ey;
 
-    public static Shape create(int typecode, int startx, int starty, int endx, int endy) {
-        return new Shape(typecode, startx, starty, endx, endy);
+    public static Shape createLine(int startx, int starty, int endx, int endy) {
+        return new ShapeLine(startx, starty, endx, endy);
     }
 
-    private Shape(int typecode, int sx, int sy, int ex, int ey) {
-        this.typecode = typecode;
+    public static Shape createRectangle(int startx, int starty, int endx, int endy) {
+        return new ShapeRectangle(startx, starty, endx, endy);
+    }
+
+    public static Shape createOval(int startx, int starty, int endx, int endy) {
+        return new ShapeOval(startx, starty, endx, endy);
+    }
+
+    protected Shape(int sx, int sy, int ex, int ey) {
         this.sx = sy;
         this.sy = sy;
         this.ex = ex;
         this.ey = ey;
     }
 
-    public int getTypecode() {
-        return this.typecode;
-    }
-
-    public String getName() {
-        switch (this.typecode) {
-            case TYPECODE_LINE:
-                return "LINE";
-            case TYPECODE_RECTANGLE:
-                return "RECTANGLE";
-            case TYPECODE_OVAL:
-                return "OVAL";
-            default:
-                return null;
-        }
-    }
+    public abstract String getName();
 
     public String toString() {
         return "[ "
@@ -46,40 +81,14 @@ class Shape {
                 + "(" + this.ex + ", " + this.ey + ") ]";
     }
 
-    public void draw() {
-        switch (this.typecode) {
-            case TYPECODE_LINE:
-                drawLine();
-                break;
-            case TYPECODE_RECTANGLE:
-                drawRectangle();
-                break;
-            case TYPECODE_OVAL:
-                drawOval();
-                break;
-            default:
-                ;
-        }
-    }
-
-    private void drawLine() {
-        System.out.println("drawLine: " + this.toString());
-    }
-
-    private void drawRectangle() {
-        System.out.println("drawRectangle: " + this.toString());
-    }
-
-    private void drawOval() {
-        System.out.println("drawOval: " + this.toString());
-    }
+    public abstract void draw();
 }
 
 public class StudyJava {
     public static void main(String[] args) {
-        Shape line = Shape.create(Shape.TYPECODE_LINE, 0,0, 10, 10);
-        Shape rectangle = Shape.create(Shape.TYPECODE_RECTANGLE, 10,20,30,40);
-        Shape oval = Shape.create(Shape.TYPECODE_OVAL, 100,200,300,400);
+        Shape line = Shape.createLine(0,0, 10, 10);
+        Shape rectangle = Shape.createRectangle(10,20,30,40);
+        Shape oval = Shape.createOval(100,200,300,400);
 
         Shape[] shape = {
                 line,
