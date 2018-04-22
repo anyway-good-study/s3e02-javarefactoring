bcbe633d318589143bc0043c0f3a16e4d14a0fcb
psu10 <psu10@Desktop-Augutus>
2018-04-22 22:22:13 +0900

    initial commit

---
+++ inheritancetodelegate/src/main/java/com/java/refactoring/inheritancetodelegate/App.java
@@ -0,0 +1,13 @@
+package com.java.refactoring.inheritancetodelegate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Delegate!" );
+    }
+}
